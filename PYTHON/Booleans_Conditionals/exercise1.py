a19d27d5d6ea402f144ed9fdd08df1003d48d3d1
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2023-09-11 16:23:15 -0600

    create python

---
+++ PYTHON/Booleans_Conditionals/exercise1.py
@@ -0,0 +1,11 @@
+# Your code goes here. Define a function called 'sign'
+def sign(n):
+    if (n < 0):
+        return -1
+    elif (n > 0):
+        return 1
+    elif (n == 0):
+        return 0
+                
+
+# Check your answer
